f5bb5aee38022e91a55893a7ed45fdd0fa5f2659
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-07-13 20:26:50 -0300

    URI_Iniciante_1059_NumerosPares.

---
+++ URI_Iniciante_1059_NumerosPares/URI_Iniciante_1059_NumerosPares/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace URI_Iniciante_1059_NumerosPares {
+    class Program {
+        static void Main(string[] args) {
+
+            for (int i = 1; i <= 100; i++) {
+                if(i%2==0){
+                    Console.WriteLine(i);
+                }
+               
+            }
+
+        }
+    }
+}

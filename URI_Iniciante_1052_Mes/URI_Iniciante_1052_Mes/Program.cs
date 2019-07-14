944f528b1767e549fe422977796fe66cbf656287
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-07-14 17:36:31 -0300

    URI_Iniciante_1052_Mes.

---
+++ URI_Iniciante_1052_Mes/URI_Iniciante_1052_Mes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI_Iniciante_1052_Mes {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            if (N == 1) { Console.WriteLine("January");}
+            else if (N == 2) { Console.WriteLine("February"); }
+            else if (N == 3) { Console.WriteLine("March"); }
+            else if (N == 4) { Console.WriteLine("April"); }
+            else if (N == 5) { Console.WriteLine("May"); }
+            else if (N == 6) { Console.WriteLine("June"); }
+            else if (N == 7) { Console.WriteLine("July"); }
+            else if (N == 8) { Console.WriteLine("August"); }
+            else if (N == 9) { Console.WriteLine("September"); }
+            else if (N == 10) { Console.WriteLine("October"); }
+            else if (N == 11) { Console.WriteLine("November"); }
+            else if (N == 12) { Console.WriteLine("December"); }
+
+        }
+    }
+}

99129c2435f92c46c123caf2f8e061422846dc68
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-07-13 23:35:45 -0300

    0052_EncapsulamentoProdutoEstoque.

---
+++ 0052_EncapsulamentoProdutoEstoque/0052_EncapsulamentoProdutoEstoque/Produto.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+
+namespace _0052_EncapsulamentoProdutoEstoque {
+    class Produto {
+
+        /*ATRIBUTOS DA CLASSE PRODUTO*/
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+        /*Construindo CONSTRUTORES: 
+         * Regra01: Definido depois dos atributos.
+           Regra02: Recebe o mesmo nome da Classe.
+           Regra03: Definir se recebe ou não parametros.*/
+
+        /*EXEMPLO DE CONSTRUTOR*/
+        public Produto() { }
+
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 50;
+        }
+        public double ValorTotalEmEstoque() {
+            return _preco * _quantidade;
+        }
+        //void=vazio: S/retorno
+        //int quantidade: dados de entrada
+        public void AdicionarProdutos(int quantidade) {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {
+            _quantidade -= quantidade;
+        }
+
+        public string GetNome() {
+            return _nome;
+        }
+        public double GetPreco() {
+            return _preco;
+        }
+        public int GetQuantidade() {
+            return _quantidade;
+        }
+        public void SetNome(string nome) {
+
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+        }
+        public override string ToString() {
+            return _nome
+                + ", $"
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " UND, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 0052_EncapsulamentoProdutoEstoque/0052_EncapsulamentoProdutoEstoque/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+namespace _0052_EncapsulamentoProdutoEstoque {
+    class Program {
+        static void Main(string[] args) {
+            //MANUAL
+            Produto p = new Produto("TV", 500, 10);
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+        }
+    }
+}

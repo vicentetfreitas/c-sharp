aa3c2eee71645d384c985120cb15063456bc39ac
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-07-19 11:14:53 -0300

    Restaurando Projeto 1040.

--- URI_Iniciante_1040_Media_3/URI_Iniciante_1040_Media_3/Program.cs
+++ URI_Iniciante_1040_Media_3/URI_Iniciante_1040_Media_3/Program.cs
@@ -5,16 +5,21 @@ namespace URI_Iniciante_1040_Media_3 {
         static void Main(string[] args) {
 
             double n1, n2, n3, n4, media, nEx, medFinal;
+
             string[] notas;
+
             notas = Console.ReadLine().Split(' ');
+
             n1 = double.Parse(notas[0], CultureInfo.InvariantCulture);
             n2 = double.Parse(notas[1], CultureInfo.InvariantCulture);
             n3 = double.Parse(notas[2], CultureInfo.InvariantCulture);
             n4 = double.Parse(notas[3], CultureInfo.InvariantCulture);
 
-            media = ((n1 * 2.0) + (n2 * 3.0) + (n3 * 4.0) + n4) / 10;
 
-            Console.WriteLine("Media: " + media.ToString(CultureInfo.InvariantCulture).Substring(0, media.ToString("F2").Length - 1));
+            media = ((n1 * 2.0) + (n2 * 3.0) + (n3 * 4.0) + n4) / 10.0;
+            
+
+            Console.WriteLine($"Media: {media.ToString(CultureInfo.InvariantCulture)}");
 
             if (media >= 7.0) {
                 Console.WriteLine("Aluno aprovado.");
@@ -25,18 +30,15 @@ namespace URI_Iniciante_1040_Media_3 {
             else if (media >= 5.0 && media <= 6.9) {
                 Console.WriteLine("Aluno em exame.");
                 nEx = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.WriteLine("Nota do exame: " + nEx.ToString(CultureInfo.InvariantCulture).Substring(0, media.ToString("F2").Length - 1));
-
                 medFinal = (media + nEx) / 2.0;
                 if (medFinal >= 5.0) {
                     Console.WriteLine("Aluno aprovado.");
-                    Console.WriteLine("Media final: " + medFinal.ToString(CultureInfo.InvariantCulture).Substring(0, media.ToString("F2").Length - 1));
+                    Console.WriteLine("Media final: " + medFinal.ToString(CultureInfo.InvariantCulture).Substring(0, media.ToString("F0").Length - 1));
 
                 }
                 else if (medFinal <= 4.9) {
                     Console.WriteLine("Aluno reprovado.");
-                    Console.WriteLine("Media final: " + medFinal.ToString(CultureInfo.InvariantCulture).Substring(0, media.ToString("F2").Length - 1));
-
+                    Console.WriteLine("Media final: " + medFinal.ToString(CultureInfo.InvariantCulture).Substring(0, media.ToString("F0").Length - 1));
                 }
 
             }

a93282fdd5f93c1022098f3ed84932c01e218f87
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-07-22 09:10:00 -0300

    0058_CalculadoraSoma.

---
+++ 0058_CalculadoraSoma/0058_CalculadoraSoma/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _0058_CalculadoraSoma {
+    class Program {
+        static void Main(string[] args) {
+
+            //int s1 = Calculadora.Soma(new int[] { 2, 3 });
+            //int s2 = Calculadora.Soma(new int[] { 2, 4, 3 });
+
+            int s1 = Calculadora.Soma(2,3);
+            int s2 = Calculadora.Soma(10,20,30,40,50);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}

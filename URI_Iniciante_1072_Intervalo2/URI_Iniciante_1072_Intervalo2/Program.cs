60d0b5b88f0895b2ae47cb312c0ee62bc4a7d617
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-07-25 03:11:59 -0300

    URI_Iniciante_1072_Intervalo2.

---
+++ URI_Iniciante_1072_Intervalo2/URI_Iniciante_1072_Intervalo2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI_Iniciante_1072_Intervalo2 {
+    class Program {
+        static void Main(string[] args) {
+            int i, n, x, contIn = 0, contOut = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < n; i++) {
+                x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20) {
+                    contIn++;
+                }
+                else {
+                    contOut++;
+                }
+            }
+            Console.WriteLine($"{contIn} in");
+            Console.WriteLine($"{contOut} out");
+        }
+    }
+}

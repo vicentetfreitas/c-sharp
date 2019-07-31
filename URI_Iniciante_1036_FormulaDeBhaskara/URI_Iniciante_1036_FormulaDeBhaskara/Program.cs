1dd524d016c841a7a2cb00e50907d7eb95cc2245
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-07-31 09:51:46 -0300

    URI_Iniciante_1036_FormulaDeBhaskara: Correção de Ponto Flutuante.

--- URI_Iniciante_1036_FormulaDeBhaskara/URI_Iniciante_1036_FormulaDeBhaskara/Program.cs
+++ URI_Iniciante_1036_FormulaDeBhaskara/URI_Iniciante_1036_FormulaDeBhaskara/Program.cs
@@ -5,29 +5,26 @@ namespace URI_Iniciante_1036_FormulaDeBhaskara {
     class Program {
         static void Main(string[] args) {
 
-            string[] coeficientes = Console.ReadLine().Split(' ');
+            string[] coeficiente = Console.ReadLine().Split(' ');
 
-            double a = double.Parse(coeficientes[0], CultureInfo.InvariantCulture);
-            double b = double.Parse(coeficientes[1], CultureInfo.InvariantCulture);
-            double c = double.Parse(coeficientes[2], CultureInfo.InvariantCulture);
+            double a = double.Parse(coeficiente[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(coeficiente[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(coeficiente[2], CultureInfo.InvariantCulture);
+            
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
 
-
-            double delta = Math.Pow(b, 2) - (4 * a * c);
             double x1, x2;
 
             if (delta < 0 || a == 0) {
                 Console.WriteLine("Impossivel calcular");
             }
             else {
-               delta= Math.Sqrt(delta);
+                x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
 
-                x1 = (-b + delta)/ 2 * a;
-
-                x2 = -b - Math.Sqrt(delta) / 2 * b;
-                Console.WriteLine("R1 = " + x1.ToString("F5"), CultureInfo.InvariantCulture);
-                Console.WriteLine("R2 = " + x2.ToString("F5"), CultureInfo.InvariantCulture);
+                Console.WriteLine("R1 = " + x1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + x2.ToString("F5", CultureInfo.InvariantCulture));
             }
-
 
         }
 

89fc3b479587ce9804916788df33706ec7c0e282
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-08-02 09:27:07 -0300

    URI_Iniciante_1045_TiposDeTriangulo: correção de testes lógicos.

--- URI_Iniciante_1045_TiposDeTriangulo/URI_Iniciante_1045_TiposDeTriangulo/Program.cs
+++ URI_Iniciante_1045_TiposDeTriangulo/URI_Iniciante_1045_TiposDeTriangulo/Program.cs
@@ -5,68 +5,68 @@ namespace URI_Iniciante_1045_TiposDeTriangulo {
     class Program {
         static void Main(string[] args) {
 
-            double a, b, c;
+            double num1, num2, num3;
             string[] valores = Console.ReadLine().Split(' ');
 
-            a = double.Parse(valores[0], CultureInfo.InvariantCulture);
-            b = double.Parse(valores[1], CultureInfo.InvariantCulture);
-            c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            num1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            num2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            num3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            double a, b, c;
 
             //Ordem Decrescente
-            if (a > b && a > c) {
-
-                //se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
-                if (a >= (b + c)) {
-                    Console.WriteLine("NAO FORMA TRIANGULO");
+            //Se num1 = A(maior)
+            if (num1 > num2 && num1 > num3) {
+                a = num1;
+                if (num2 > num3) {
+                    b = num2;
+                    c = num3;
                 }
-                //se A2 = B2 + C2, apresente a mensagem: TRIANGULO RETANGULO
-                if (Math.Pow(a, 2) == ((Math.Pow(b, 2)) + (Math.Pow(c, 2)))) {
-                    Console.WriteLine("TRIANGULO RETANGULO");
+                else {
+                    b = num3;
+                    c = num2;
                 }
-                //se A2 > B2 + C2, apresente a mensagem: TRIANGULO OBTUSANGULO
-                if (Math.Pow(a, 2) > ((Math.Pow(b, 2)) + (Math.Pow(c, 2)))) {
-                    Console.WriteLine("TRIANGULO OBTUSANGULO");
-                }
-                if ((b * b) < (a * a) + (c * c) || (c * c) < (a * a) + (b * b)) {
-                    Console.WriteLine("TRIANGULO ACUTANGULO");
-                }
-
             }
-            else if (b > a && b > c) {
-
-
-                //se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
-                if (b >= (a + c)) {
-                    Console.WriteLine("NAO FORMA TRIANGULO");
+            else if (num2 > num1 && num2 > num3) {
+                a = num2;
+                if (num1 > num3) {
+                    b = num1;
+                    c = num3;
                 }
-                //se B2 = A2 + B2, apresente a mensagem: TRIANGULO RETANGULO
-                if (Math.Pow(b, 2) == ((Math.Pow(a, 2)) + (Math.Pow(b, 2)))) {
-                    Console.WriteLine("TRIANGULO RETANGULO");
+                else {
+                    b = num3;
+                    c = num1;
                 }
-                //se A2 > B2 + C2, apresente a mensagem: TRIANGULO OBTUSANGULO
-                if (Math.Pow(b, 2) > ((Math.Pow(a, 2)) + (Math.Pow(b, 2)))) {
-                    Console.WriteLine("TRIANGULO OBTUSANGULO");
-                }
-                if ((a * a) < (b * b) + (c * c) || (c * c) < (a * a) + (b * b)) {
-                    Console.WriteLine("TRIANGULO ACUTANGULO");
-                }
-
             }
-            else if (c > a && c > b) {
-
-                //se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
-                if (c >= (b + a)) {
-                    Console.WriteLine("NAO FORMA TRIANGULO");
+            else {
+                a = num3;
+                if (num1 > num2) {
+                    b = num1;
+                    c = num2;
                 }
-                //se C2 = A2 + B2, apresente a mensagem: TRIANGULO RETANGULO
-                if (Math.Pow(c, 2) == ((Math.Pow(b, 2)) + (Math.Pow(a, 2)))) {
+                else {
+                    b = num2;
+                    c = num1;
+                }
+            }
+            if (a >= b + c) {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            else {
+                if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2)) {
                     Console.WriteLine("TRIANGULO RETANGULO");
                 }
-                if (Math.Pow(c, 2) > ((Math.Pow(b, 2)) + (Math.Pow(a, 2)))) {
+                else if (Math.Pow(a, 2) > Math.Pow(b, 2) + Math.Pow(c, 2)) {
                     Console.WriteLine("TRIANGULO OBTUSANGULO");
                 }
-                if ((a * a) < (b * b) + (c * c) || (b * b) < (a * a) + (c * c)) {
+                else {
                     Console.WriteLine("TRIANGULO ACUTANGULO");
+                }
+                if (a == b && b == c) {
+                    Console.WriteLine("TRIANGULO EQUILATERO");
+                }
+                else if (a == b || a == c || b == c) {
+                    Console.WriteLine("TRIANGULO ISOSCELES");
                 }
 
             }

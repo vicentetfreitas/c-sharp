0814946ffe0eaa67836427df3f5fd4f59c5dcf57
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-08-04 14:36:00 -0300

    URI_Iniciante_1113_CrescenteDecrescente.

---
+++ URI_Iniciante_1113_CrescenteDecrescente/URI_Iniciante_1113_CrescenteDecrescente/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace URI_Iniciante_1113_CrescenteDecrescente {
+    class Program {
+        static void Main(string[] args) {
+
+            int x, y;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            x = int.Parse(valores[0]);
+            y = int.Parse(valores[1]);
+            while (x != y) {
+                if (x < y) {
+                    Console.WriteLine("Crescente");
+                }
+                else {
+                    Console.WriteLine("Decrescente");
+                }
+                valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+            }
+
+
+        }
+    }
+}

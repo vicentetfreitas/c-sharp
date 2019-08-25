202ca0555f4fc962620d8c3b80b4b421b422fe92
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-08-24 23:17:23 -0300

    0072_ExpressaoCondicionalTernaria.

---
+++ 0072_ExpressaoCondicionalTernaria/0072_ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace _0072_ExpressaoCondicionalTernaria {
+    class Program {
+        static void Main(string[] args) {
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            //if (preco < 20.0) {
+            //    desconto = preco * 0.1;
+            //}
+            //else {
+            //    desconto = preco * 0.05;
+            //}
+
+            Console.WriteLine(desconto);
+        }
+    }
+}

1a4e5d935ae1d55fa71769eae84f2d232918c488
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-08-25 21:45:48 -0300

    0081_DemoStringBuilder.

---
+++ 0081_DemoStringBuilder/0081_DemoStringBuilder/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿
+namespace _0081_DemoStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}

df8d4960401d93b594acd9401defa54bde4dc95d
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-08-27 07:36:39 -0300

    0082_Order.

---
+++ 0082_Order/0082_Order/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace _0082_Order.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ 0082_Order/0082_Order/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+using System.Collections.Generic;
+using _0082_Order.Entities;
+using _0082_Order.Entities.Enums;
+
+namespace _0082_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+        }
+    }
+}

6c9d04fc5f71d59e4bd96bccf03ee04ed26fa9c8
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2019-09-02 07:39:27 -0300

    0083_Heritage.

---
+++ 0083_Heritage/0083_Heritage/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0083_Heritage.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void Widthdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ 0083_Heritage/0083_Heritage/Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _0083_Heritage.Entities;
+
+namespace _0083_Heritage.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+        public BusinessAccount() { }
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+            else { }
+
+
+        }
+    }
+}
